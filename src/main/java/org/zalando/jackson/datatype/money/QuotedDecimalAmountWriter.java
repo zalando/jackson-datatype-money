74090a9fd9577e85c83616ae01eea2a145c6914c
Willi Schönborn <w.schoenborn@gmail.com>
2017-09-01 10:50:26 +0200

    Simplified quoted decimal implementation

--- src/main/java/org/zalando/jackson/datatype/money/QuotedDecimalAmountWriter.java
+++ src/main/java/org/zalando/jackson/datatype/money/QuotedDecimalAmountWriter.java
@@ -1,23 +1,14 @@
 package org.zalando.jackson.datatype.money;
 
 import javax.money.MonetaryAmount;
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
 
 final class QuotedDecimalAmountWriter implements AmountWriter {
 
+    private final DecimalAmountWriter delegate = new DecimalAmountWriter();
+
     @Override
     public String write(final MonetaryAmount amount) {
-        final BigDecimal decimal = amount.getNumber().numberValueExact(BigDecimal.class);
-        final int defaultFractionDigits = amount.getCurrency().getDefaultFractionDigits();
-        final int scale = Math.max(decimal.scale(), defaultFractionDigits);
-
-        final DecimalFormat format = new DecimalFormat();
-        format.setMinimumFractionDigits(defaultFractionDigits);
-        format.setMaximumFractionDigits(scale);
-        format.setGroupingUsed(false);
-
-        return format.format(decimal);
+        return delegate.write(amount).toPlainString();
     }
 
 }
